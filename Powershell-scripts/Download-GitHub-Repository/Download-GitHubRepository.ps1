a5d62a63b93c906299a8f7f2da5652ef8d5b36e0
Sandro Pereira <sandro-pereira@live.com.pt>
2020-09-01 23:21:46 +0100

    Download GitHub Repository

---
+++ Powershell-scripts/Download-GitHub-Repository/Download-GitHubRepository.ps1
@@ -0,0 +1,45 @@
+﻿######################################################################
+#                                                                    #
+# Download and Unzip GitHub Repository                               #
+# Author: Sandro Pereira                                             #
+#                                                                    #
+######################################################################
+
+function DownloadGitHubRepository 
+{ 
+    param( 
+       [Parameter(Mandatory=$True)] 
+       [string] $Name, 
+        
+       [Parameter(Mandatory=$True)] 
+       [string] $Author, 
+        
+       [Parameter(Mandatory=$False)] 
+       [string] $Branch = "master", 
+        
+       [Parameter(Mandatory=$False)] 
+       [string] $Location = "c:\temp" 
+    ) 
+    
+    # Force to create a zip file 
+    $ZipFile = "$location\$Name.zip" 
+    New-Item $ZipFile -ItemType File -Force
+
+    #$RepositoryZipUrl = "https://github.com/sandroasp/Microsoft-Integration-and-Azure-Stencils-Pack-for-Visio/archive/master.zip"
+    $RepositoryZipUrl = "https://api.github.com/repos/$Author/$Name/zipball/$Branch"  
+    # download the zip 
+    Write-Host 'Starting downloading the GitHub Repository'
+    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile $ZipFile
+    Write-Host 'Download finished'
+
+    #Extract Zip File
+    Write-Host 'Starting unziping the GitHub Repository locally'
+    Expand-Archive -Path $ZipFile -DestinationPath $location -Force
+    Write-Host 'Unzip finished'
+    
+    # remove zip file
+    Remove-Item -Path $ZipFile -Force 
+}
+
+[String]$location = Split-Path -Parent $PSCommandPath
+DownloadGithubRepository -Name 'Microsoft-Integration-and-Azure-Stencils-Pack-for-Visio' -Author 'sandroasp' -Location $location
---
+++ Powershell-scripts/Download-GitHub-Repository/README.md
@@ -0,0 +1,8 @@
+# How to Download a GitHub Repository with PowerShell
+This is a PowerShell function to download a full GitHub repository and unzip it locally.
+Because we are downloading the full repository in a Zip file we avoid reaching API Rate limit (60 requests per hour for unauthenticated requests or 5000 requests per hour if you use Basic Authentication or OAuth)
+ 
+THIS POWERSHELL IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+# About Me
+**Sandro Pereira** | [DevScope](http://www.devscope.net/) | MVP & MCTS BizTalk Server 2010 | [https://blog.sandro-pereira.com/](https://blog.sandro-pereira.com/) | [@sandro_asp](https://twitter.com/sandro_asp)
