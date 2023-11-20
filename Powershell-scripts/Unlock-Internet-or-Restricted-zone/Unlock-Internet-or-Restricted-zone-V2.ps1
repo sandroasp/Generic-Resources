dfb142b0e2cacce671f645ac69567289c01ad17a
Sandro Pereira <sandro-pereira@live.com.pt>
2023-11-20 11:34:05 +0000

    unblock

---
+++ Powershell-scripts/Unlock-Internet-or-Restricted-zone/Unlock-Internet-or-Restricted-zone-V2.ps1
@@ -0,0 +1,5 @@
+﻿# STEP 1 
+# Defining the name of the project folder
+[string]$projDrive = Read-Host -Prompt "Please enter the project location: " 
+
+Get-ChildItem $projDrive -Recurse | %{ Unblock-File $_.FullName }
